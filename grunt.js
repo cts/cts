f13ed1b3fdeb9232d2ccc7ea2e6e87da42c050c2
Edward Benson <edward.benson@gmail.com>
2013-01-28 11:46:34 -0500

    linted

--- grunt.js
+++ grunt.js
@@ -11,14 +11,14 @@ module.exports = function(grunt) {
       banner: "/**\n" +
               "* <%= pkg.name %>\n" +
               " * <%= pkg.description %>\n" +
-			        " *\n" +
-			        " * @author <%= pkg.author.name %> <<%= pkg.author.email %>>\n" +
-			        " * @copyright <%= pkg.author.name %> <%= grunt.template.today('yyyy') %>\n" +
-			        " * @license <%= pkg.licenses[0].type %> <<%= pkg.licenses[0].url %>>\n" +
-			        " * @link <%= pkg.homepage %>\n" +
-			        " * @module <%= pkg.name %>\n" +
-			        " * @version <%= pkg.version %>\n" +
-			        " */"
+              " *\n" +
+              " * @author <%= pkg.author.name %> <<%= pkg.author.email %>>\n" +
+              " * @copyright <%= pkg.author.name %> <%= grunt.template.today('yyyy') %>\n" +
+              " * @license <%= pkg.licenses[0].type %> <<%= pkg.licenses[0].url %>>\n" +
+              " * @link <%= pkg.homepage %>\n" +
+              " * @module <%= pkg.name %>\n" +
+              " * @version <%= pkg.version %>\n" +
+              " */"
     },
     concat: {
       dist : {
