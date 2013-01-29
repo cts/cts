b50025b57cc9719d5e333feab298b395ba906db0
Ted Benson <edward.benson@gmail.com>
2013-01-29 13:49:05 -0500

    tests

---
+++ test/specs/are.test.js
@@ -0,0 +1,31 @@
+module("are", {
+  setup : function () {
+    //this.a = $("<div id='a'></div>").appendTo($("body"));
+    //this.b = $("<div id='b'></div>").appendTo($("body"));
+	},
+	teardown : function () {
+    //this.a.remove();
+    //this.a = null;
+    //this.b.remove();
+    //this.b = null;
+	}
+});
+
+test("ARE aligns cardinalities down", function () {
+  ok(1 == 1, "Passed"); 
+	//deepEqual(A, B, "should be equal");
+  //this.a.html("<ul data-cts='are: b;'><li itemscope>Foo</li><li itemscope>Bar</li><li itemscope>Baz</li></ul>");
+  //this.b.html("<div itemscope>Foo</div>");
+  //var engine = new CTS.Engine();
+  //engine.render();
+});
+
+test("ARE aligns cardinalities up", function () {
+  ok(1 == 1, "Passed"); 
+	//deepEqual(A, B, "should be equal");
+});
+
+test("ARE doesn't touch even cardinatlies", function () {
+  ok(1 == 1, "Passed"); 
+	//deepEqual(A, B, "should be equal");
+});
