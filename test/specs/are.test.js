ee5ce5e75d0f7713789442b3a43968c633120675
Ted Benson <edward.benson@gmail.com>
2013-03-11 12:58:20 -0400

    are condition

--- test/specs/are.test.js
+++ test/specs/are.test.js
@@ -3,8 +3,7 @@ module("are", {
     this.a = $("<ul id='a'></ul>").appendTo($("body"));
     this.b = $("<div id='b'></div>").appendTo($("body"));
 	},
-	teardown : function () {
-    this.a.remove();
+	teardown : function () { this.a.remove();
     this.a = null;
     this.b.remove();
     this.b = null;
@@ -73,11 +72,33 @@ asyncTest("ARE aligns cardinalities up", function () {
   });
 });
 
+test("ARE doesn't touch even cardinatlies", function () {
+  this.a.attr('data-cts', 'are: #b;');
+  this.a.html('<li>Foo</li><li>Bar</li>');
+  this.b.html("<div>Foo</div><div>Foo</div>");
 
-//test("ARE aligns cardinalities up", function () {
-//  ok(1 == 1, "Passed"); 
-//});
-//
-//test("ARE doesn't touch even cardinatlies", function () {
-//  ok(1 == 1, "Passed"); 
-//});
+  var engine = new CTS.Engine();
+  engine.render({
+    callback: function() {
+      var tree = engine.forrest.trees['body'];
+      var body = tree.root;
+      console.log('body', body);
+      var bodyKids = body.getChildren();
+      equal(bodyKids.length, 1, "only one child of body");
+      var A = bodyKids[0];
+      equal(A.relations.length, 1, "A has one relation");
+      var r = A.relations[0];
+      equal(r.name, "are", "that relation is ARE");
+      equal(r.selection1.nodes[0], A, "R selection 1 is A");
+      equal(r.selection2.nodes.length, 1, "R.S2 is len 1");
+      var other = r.selection2.nodes[0];
+      equal(other.getRelations().length, 1, "other has one relation");
+      equal(r.selection2.nodes[0].getChildren().length, 2, "R.S2 has 2 children");
+      var aKids = A.getChildren();
+      equal(aKids.length, 2, "Should only be two li's in a");
+      equal(A.siblings[0].html(), "<li>Foo</li><li>Bar</li>", "three LI html");
+      start();
+    },
+    callbackScope: this
+  });
+});
