cedb43bf9b76bcbe737f727546b36858b4dabb3d
Ted Benson <edward.benson@gmail.com>
2013-05-03 13:27:58 -0800

    selec

---
+++ src/model/tree-scraper.js
@@ -0,0 +1,12 @@
+/* Like a JSON tree but any CTS rules CREATE the keypath upon resolution.
+ */
+var ScraperTree = CTS.ScraperTree = function(forrest, attributes) {
+  this.forrest = forrest;
+  this.root = {};
+};
+
+// Instance Methods
+// ----------------
+_.extend(JsonTree.prototype, Tree, {
+
+});
