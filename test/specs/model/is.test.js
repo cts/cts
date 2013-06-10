71cad8b23df01f96e9a69da5ba5fa1c6bdd14834
Ted Benson <edward.benson@gmail.com>
2013-06-10 15:41:21 -0400

    bug in peer children

--- test/specs/model/is.test.js
+++ test/specs/model/is.test.js
@@ -9,6 +9,7 @@ test("IS replaces value", function() {
   equal(CTS.Debugging.QuickTest("a", "b", "a is b"), "b");
   equal(CTS.Debugging.QuickTest("a(b)", "c", "b is c"), "a(c)");
   equal(CTS.Debugging.QuickTest("a(b c d)", "e", "d is e"), "a(b c e)");
+  equal(CTS.Debugging.QuickTest("a(b c d)", "e(f(g))", "d is g"), "a(b c g)");
 });
 
 //asyncTest("IS text -> text on same page", function () {
