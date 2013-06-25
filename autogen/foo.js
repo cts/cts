edfa99a298a8e107e4e055756ba4317ea5487097
Ted Benson <edward.benson@gmail.com>
2013-06-25 10:36:39 -0400

    remove autogen

--- autogen/foo.js
+++
@@ -1,8 +0,0 @@
-var parser = require("./cts2-parser.js").parser
-
-parser.parse("@tree a b url(http://www.google.com); \n a is b;")
-
-parser.parse("@tree a b url(http://www.google.com); at|a {ap} is {ip} bt|b {bp};");
-
-
-parser.parse("@tree a b url(http://www.google.com); \n a {ap} is {ip} bt|b {bp};")
