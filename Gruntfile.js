8f7c7ad953ddea9a493909bfd78ff78f54c6eb19
Ted Benson <edward.benson@gmail.com>
2013-09-27 01:25:52 -0400

    Split out webserver into own NPM package. Yay!

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,12 @@
  * To be used with GruntJS <http://gruntjs.com/>
  */
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-web-server');
+
   // Project configuration.
   grunt.initConfig({
     pkg: "<json:package.json>",
@@ -21,6 +27,15 @@ module.exports = function(grunt) {
               " * @module <%= pkg.name %>\n" +
               " * @version <%= pkg.version %>\n" +
               " */"
+    },
+    web_server: {
+      whyisthisnecessary: 'idontknow',
+      options: {
+        cors: true,
+        port: 9000,
+        logRequests: true,
+        nevercache: true
+      }
     },
     concat: {
       dist : {
@@ -126,71 +141,6 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-
-  require('./grunt-contrib-http-server')(grunt);
-
-  grunt.registerTask('default', [
-    'jshint',
-    //'jison', -- too 
-    'concat']);
-
-  /*
-   *
-   */
-  grunt.registerTask('serve', 'Serves the project.', function (env) {
-    var http = require('http'),
-        url  = require('url'),
-        path = require('path'),
-        fs   = require('fs');
-
-    var mimeTypes = {
-        "html": "text/html",
-        "jpeg": "image/jpeg",
-        "jpg" : "image/jpeg",
-        "png" : "image/png",
-        "js"  : "text/javascript",
-        "css" : "text/css"};
-
-    http.createServer(function(req, res) {
-      var uri = url.parse(req.url).pathname;
-      var filename = path.join(process.cwd(), unescape(uri));
-      var stats;
-    
-      try {
-        stats = fs.lstatSync(filename); // throws if path doesn't exist
-      } catch (e) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('404 Not Found\n');
-        res.end();
-        return;
-      }
-    
-      if (stats.isFile()) {
-        // path exists, is a file
-        var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
-        res.writeHead(200, {'Content-Type': mimeType} );
-    
-        var fileStream = fs.createReadStream(filename);
-        fileStream.pipe(res);
-      } else if (stats.isDirectory()) {
-        // path exists, is a directory
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.write('Index of '+uri+'\n');
-        res.write('TODO, show index?\n');
-        res.end();
-      } else {
-        // Symbolic link, other?
-        // TODO: follow symlinks?  security?
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.write('500 Internal server error\n');
-        res.end();
-        // This is a test
-      }
-    }).listen(1337);
-  });
+  grunt.registerTask('default', ['jshint', 'concat']);
 
 };
