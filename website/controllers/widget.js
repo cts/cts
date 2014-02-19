eec1de1c5e107fffbbe15e79153a9e62ca70d05a
Ted Benson <edward.benson@gmail.com>
2014-02-19 17:24:18 -0500

    widgets index

--- website/controllers/widget.js
+++ website/controllers/widget.js
@@ -1,15 +1,16 @@
-/**
- * GET /
- * Home page.
- */
+var path = require('path');
 
 exports.index = function(req, res) {
-  res.render('cts/widgets/index', {});
+  var widgets = require(path.join(__dirname, '..', 'static', 'widgets', 'index.json'));
+  for (var i = 0; i < widgets.length; i++) {
+    widgets[i]['thumbnailUrl'] = '/widgets/' + widgets[i].id + '/' + widgets[i].id + '.png';
+    widgets[i]['url'] = '/widgets/' + widgets[i].id + '/';
+  }
+  res.render('cts/widgets/index', {widgets: widgets});
 };
 
 exports.show = function(req, res) {
   var widget = req.params.widget;
   res.render('cts/widgets/show', {});
 };
-
 
