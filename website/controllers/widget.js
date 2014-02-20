5182f0c74cdc0d452565bd40be6f279d678c20a5
Ted Benson <edward.benson@gmail.com>
2014-02-19 20:17:43 -0500

    bubble and choro

--- website/controllers/widget.js
+++ website/controllers/widget.js
@@ -1,16 +1,30 @@
 var path = require('path');
+var fs = require('fs');
 
 exports.index = function(req, res) {
   var widgets = require(path.join(__dirname, '..', 'static', 'widgets', 'index.json'));
   for (var i = 0; i < widgets.length; i++) {
     widgets[i]['thumbnailUrl'] = '/widgets/' + widgets[i].id + '/' + widgets[i].id + '.png';
-    widgets[i]['url'] = '/widgets/' + widgets[i].id + '/';
+    widgets[i]['url'] = '/cts/widgets/' + widgets[i].id + '/';
   }
   res.render('cts/widgets/index', {widgets: widgets});
 };
 
 exports.show = function(req, res) {
-  var widget = req.params.widget;
-  res.render('cts/widgets/show', {});
+  var widgetId = req.params.widget;
+  var dataFile =  path.join(__dirname, '..', 'static', 'widgets', widgetId, 'index.json');
+  var exampleFile =  path.join(__dirname, '..', 'static', 'widgets', widgetId, 'tutorial.html');
+
+  var widget = require(dataFile);
+  fs.readFile(exampleFile, function (err, data) {
+    if (err) throw err;
+    widget.exampleHtml = data;
+    widget['thumbnailUrl'] = '/widgets/' + widgetId + '/' + widgetId + '.png';
+    widget['bannerUrl'] = '/widgets/' + widgetId + '/banner.png';
+    widget['exampleUrl'] = '/widgets/' + widgetId + '/example.html';
+    widget['url'] = '/cts/widgets/' + widgetId + '/';
+    res.render('cts/widgets/show', widget);
+  });
+ 
 };
 
