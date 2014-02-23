e83d5425334b048d62ea8ad5423adadd0c5762f5
Ted Benson <edward.benson@gmail.com>
2014-02-23 11:49:26 -0500

    removing unused listening code from TREE

--- src/engine/model/tree/tree-dom.js
+++ src/engine/model/tree/tree-dom.js
@@ -29,28 +29,6 @@ CTS.Fn.extend(CTS.Tree.Html.prototype, CTS.Tree.Base, {
     } else {
       return ctsnode;
     }
-  },
-
-  listenForNodeInsertions: function(new_val) {
-    var listening = (this.insertionListener != null);
-    if (typeof new_val == 'undefined') {
-      return listening;
-    } else {
-      if (new_val == listening) {
-        return new_val;
-      } else if (new_val == true) {
-        // Turn on.
-        this.insertionListener = function(evt) {
-          self.root.trigger("DOMNodeInserted", evt);
-        };
-        this.root.value.on("DOMNodeInserted", this.insertionListener);
-      } else if (new_val == false) {
-        this.root.value.off("DOMNodeInserted", this.insertionListener);
-        this.insertionListener = null;
-      } else {
-        CTS.Log.Error("Invalid value passed to listenForNodeInsertions");
-      }
-    }
   }
 
 });
