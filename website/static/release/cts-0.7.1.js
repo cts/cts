dcdd26f15cf9d80dfdd17995678b74effd3a7dfb
Ted Benson <eob@csail.mit.edu>
2014-09-30 20:27:27 -0400

    bug

--- website/static/release/cts-0.7.1.js
+++ website/static/release/cts-0.7.1.js
@@ -16672,7 +16672,7 @@ Util._.extend(IfExist.prototype, Model.Relation.Base, {
     if (Util._.isUndefined(n1)) {
       n1 = this.node1;
     }
-    if (Util._isUndefined(n2)) {
+    if (Util._.isUndefined(n2)) {
       n2 = this.node2;
     }
     return new IfExist(n1, n2, this.spec);
